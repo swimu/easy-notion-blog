a91d5d6b72b450964847d2b2e56569ed13927a99
swimu <swimu1003@gmail.com>
2022-08-22 22:20:34 +0900

    タイトルの変更・トップページの内容を更新

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,29 @@ const RenderPage = () => (
     <DocumentHead />
 
     <div>
-      <h2>Welcome!</h2>
-      <p>Your easy-notion-blog deployed successfully!</p>
-      <p>Have fun!</p>
+      <h2>すいむ (@swimuu)って？</h2>
+      <p>おでかけとのりもの、おいしいごはんとおさけがだいすきなｱﾋﾙﾁｬﾝです。</p>
+      <p>昔は南の島に住んでいたけれど、今は日本のまんなかあたりにいます。</p>
+      <p>好きなゲームは陣取りゲーム、SplatoonとIngressをやってたりやってなかったりします。</p>
+      <p>好きな色は青です。</p>
+      <p>プログラミングにも興味があります。</p>
+      <p>このサイトは  <ExtLink href="https://github.com/otoyo/easy-notion-blog">easy-notion-blog</ExtLink> をいじって公開しています。</p>
+      <p>easy-notion-blog を使っている方々から刺激を受けながら、楽しんでいます。</p>
+      <p>最近はいろいろなものに興味を持ちすぎて収集付かなくなっています。</p>
+      <p>やりきれないくらい、やりたいことがいっぱいあるのは嬉しいことだなとおもっています。</p>
+      <br/>
+      <h2>このブログで書きたいこと</h2>
+      <ul>
+        <li>おでかけのこと</li>
+        <li>のりもののこと</li>
+        <li>おいしいごはんとおさけのこと</li>
+        <li>ゲームのこと</li>
+        <li>easy-notion-blogのカスタマイズのこと</li>
+        <li>そのほかTwitterでは書ききれないあれこれ</li>
+      </ul>
+      <br/>
+    <h2>連絡をとりたい</h2>
+    <p>Twitterアカウント(<ExtLink href="https://twitter.com/swimuuu">@swimuuu</ExtLink>)で待っています。</p>
       <p>
         easy-notion-blog powered by{' '}
         <ExtLink href="https://github.com/otoyo/easy-notion-blog">
